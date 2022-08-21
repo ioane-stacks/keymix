033b6450a1e408be348b3ebc43376f58b486c372
Ioane <ioane.natroshvili08b@geolab.edu.ge>
2022-08-21 14:47:23 +0400

    header

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './keymix.css';
 import '../node_modules/bpg-banner/css/bpg-banner.min.css';
+import '../node_modules/bpg-banner-caps/css/bpg-banner-caps.min.css';
 import Navbar from './Navbar';
 import Slider from './Slider';
 import TopHeader from './TopHeader';
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,8 +1,22 @@
-function Navbar() {
+import hlogo from './icons/cardprintlogo.png';
 
+function Navbar(data) {
 
     return (
-        <div className="header">NAVBAR</div>
+        <div className="header">
+            <div className="header-logo">
+                <a href='/'><img src={hlogo} alt='logo'></img></a>
+            </div>
+            <div className='navbar'>
+                <ul>
+                    <li>პროდუქცია</li>
+                    <li>მომსახურეობა</li>
+                    <li>გალერეა</li>
+                    <li>ინფორმაცია</li>
+                    <li>ფასები</li>
+                </ul>
+            </div>
+        </div>
     )
 }
 
