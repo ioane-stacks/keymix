f72b601d154a3e909b9fd32929172be514a172b5
ioane-stacks <ioane.natroshvili08b@geolab.edu.ge>
2022-08-25 03:40:36 +0400

    added multipage function

--- src/pages/Product.js
+++ src/pages/Product.js
@@ -1,12 +1,21 @@
+import { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import { defaultContent } from '../data/defaultContent.js';
 
-function Product({ data }) {
+function Product() {
 
+    const { productionMenu } = defaultContent.navbar.menubar;
     const params = useParams();
+
+
+    let product = productionMenu.filter(prod => prod.id.toString() === params.productId);;
+
+    const { id, productName } = product[0];
 
     return (
         <div>
-            <h1>PRODUCT {params.productId}</h1>
+            <h1>PRODUCT ID: {id}</h1>
+            <h1>PRODUCT NANE: {productName}</h1>
         </div>
     )
 }
