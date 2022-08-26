e30833d5caa3c44f0d7b99d15cf50d8b23035b22
Ioane <ioane.natroshvili08b@geolab.edu.ge>
2022-08-26 19:29:35 +0400

    navbar mobile

--- src/footer/Footer.js
+++ src/footer/Footer.js
@@ -1,58 +1,119 @@
 import { ImLocation2, ImFacebook, ImYoutube, ImLinkedin } from "react-icons/im";
 import { FaInstagram, FaPhoneAlt } from "react-icons/fa";
 import { FiMail } from "react-icons/fi";
-import { RiTreasureMapFill } from 'react-icons/ri';
-import { defaultContent } from '../data/defaultContent.js';
+import { RiTreasureMapFill } from "react-icons/ri";
+import { defaultContent } from "../data/defaultContent.js";
 
 function Footer() {
-    
-    const {socialPageName, facebook, instagram, linkedin, youtube} = defaultContent.footer.socialPages;
-    const {aboutUsName, termsAndConditions, termsOfUse, privacyPolicy, siteMap} = defaultContent.footer.aboutUs;
-    const {contactName, phoneNumber, email, address, findUs} = defaultContent.footer.contact;
+	const { socialPageName, facebook, instagram, linkedin, youtube } = defaultContent.footer.socialPages;
+	const { aboutUsName, termsAndConditions, termsOfUse, privacyPolicy, siteMap } = defaultContent.footer.aboutUs;
+	const { contactName, phoneNumber, email, address, findUs } = defaultContent.footer.contact;
 
-      return (
-            <div className="footer">
-                  <article>
-                        <div className="footer-name">
-                              <h2>{ socialPageName }</h2>
-                        </div>
-                        <div className="footer-content">
-                              <ul>
-                                    <li><a href="#"><div><ImFacebook /></div>{ facebook }</a></li>
-                                    <li><a href="#"><div><FaInstagram /> </div>{ instagram }</a></li>
-                                    <li><a href="#"><div><ImLinkedin /></div>{ linkedin }</a></li>
-                                    <li><a href="#"><div><ImYoutube /></div>{ youtube }</a></li>
-                              </ul>
-                        </div>
-                  </article>
-                  <article>
-                        <div className="footer-name">
-                              <h2>{ aboutUsName }</h2>
-                        </div>
-                        <div className="footer-content">
-                              <ul>
-                                    <li><a href="#">{ termsAndConditions }</a></li>
-                                    <li><a href="#">{ termsOfUse }</a></li>
-                                    <li><a href="#">{ privacyPolicy }</a></li>
-                                    <li><a href="#">{ siteMap }</a></li>
-                              </ul>
-                        </div>
-                  </article>
-                  <article>
-                        <div className="footer-name">
-                              <h2>{ contactName }</h2>
-                        </div>
-                        <div className="footer-content">
-                              <ul>
-                                    <li><a href="#"><div><FaPhoneAlt /></div>{ phoneNumber }</a></li>
-                                    <li><a href="#"><div><FiMail /> </div>{ email }</a></li>
-                                    <li><div><ImLocation2 /></div>{ address }</li>
-                                    <li><a href="#"><div><RiTreasureMapFill /></div>{ findUs }</a></li>
-                              </ul>
-                        </div>
-                  </article>
-            </div>
-      );
+	return (
+		<div className="footer">
+			<article>
+				<div className="footer-name">
+					<h2>{socialPageName}</h2>
+				</div>
+				<div className="footer-content">
+					<ul>
+						<li>
+							<a href="#">
+								<div>
+									<ImFacebook />
+								</div>
+								{facebook}
+							</a>
+						</li>
+						<li>
+							<a href="#">
+								<div>
+									<FaInstagram />
+								</div>
+								{instagram}
+							</a>
+						</li>
+						<li>
+							<a href="#">
+								<div>
+									<ImLinkedin />
+								</div>
+								{linkedin}
+							</a>
+						</li>
+						<li>
+							<a href="#">
+								<div>
+									<ImYoutube />
+								</div>
+								{youtube}
+							</a>
+						</li>
+					</ul>
+				</div>
+			</article>
+			<article>
+				<div className="footer-name">
+					<h2>{aboutUsName}</h2>
+				</div>
+				<div className="footer-content">
+					<ul>
+						<li>
+							<a href="#">{termsAndConditions}</a>
+						</li>
+						<li>
+							<a href="#">{termsOfUse}</a>
+						</li>
+						<li>
+							<a href="#">{privacyPolicy}</a>
+						</li>
+						<li>
+							<a href="#">{siteMap}</a>
+						</li>
+					</ul>
+				</div>
+			</article>
+			<article>
+				<div className="footer-name">
+					<h2>{contactName}</h2>
+				</div>
+				<div className="footer-content">
+					<ul>
+						<li>
+							<a href="#">
+								<div>
+									<FaPhoneAlt />
+								</div>
+								{phoneNumber}
+							</a>
+						</li>
+						<li>
+							<a href="#">
+								<div>
+									<FiMail />{" "}
+								</div>
+								{email}
+							</a>
+						</li>
+						<li>
+							<div>
+								<ImLocation2 />
+							</div>
+							{address}
+						</li>
+						<li>
+							<a href="#">
+								<div>
+									<RiTreasureMapFill />
+								</div>
+								{findUs}
+							</a>
+						</li>
+					</ul>
+				</div>
+			</article>
+		</div>
+	);
 }
 
 export default Footer;
--- src/pages/Service.js
+++ src/pages/Service.js
@@ -1,11 +1,13 @@
 function Service() {
-  return (
-    <div className="container">
-      <section className="page-section">
-        <div className="ps-header"><h1>სერვისი</h1></div>
-      </section>
-    </div>
-  )
+	return (
+		<div className="container">
+			<section className="page-section">
+				<div className="ps-header">
+					<h1>სერვისი</h1>
+				</div>
+			</section>
+		</div>
+	);
 }
 
 export default Service;
